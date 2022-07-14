2bbfea7c61bf0a5b6fa4801508d1d65c262ce7a9
Kenneth Amurao <kenneth.p.amurao@gmail.com>
2022-07-15 00:50:05 +1000

    Added bulk Items list

--- bulkItemsScript.js
+++ bulkItemsScript.js
@@ -20,9 +20,19 @@ $(document).ready(function() {
     var largestDifference = new Array(10).fill(0)
     var largestCostForOne = new Array(10).fill(0)
     var largest = 0;
+    
+    var tenLargest10 = new Array(10).fill(0)
+    var tenLargestName = new Array(10).fill(0)
+    var tenLargestPrice1 = new Array(10).fill(0)
+    var tenLargestPrice10 = new Array(10).fill(0)
+    var tenLargestPrice100 = new Array(10).fill(0)
+    var tenLargestDifference = new Array(10).fill(0)
+    var tenLargestCostForOne = new Array(10).fill(0)
+    var tenLargest = 0;
 
     for(var i = 0; i < data.length; i++) {
         if(data[i].date === today) {
+        //CALCULATE COST FOR 100 TABLE
             if(data[i].price100 != 0 && data[i].price1 != 0) {
                 var costForOne = parseInt((data[i].price100/100))
                 var priceDifference = parseInt(data[i].price1) - costForOne
@@ -43,36 +53,70 @@ $(document).ready(function() {
                 }
                 itemNameList.push(priceDifference)
             }
+            if(data[i].price10 != 0 && data[i].price1 != 0) {
+                var costForOne = parseInt((data[i].price10/10))
+                var priceDifference = parseInt(data[i].price1) - costForOne
+                for(var y = 0; y < largest10.length; y++) {
+                    if(priceDifference > largest10[y]) {
+                        tenLargest10[y] = priceDifference
+                        tenLargestName[y] = data[i].itemName
+                        tenLargestPrice1[y] = data[i].price1
+                        tenLargestPrice10[y] = data[i].price10
+                        tenLargestPrice100[y] = data[i].price100
+                        tenLargestDifference[y] = priceDifference
+                        tenLargestCostForOne[y] = costForOne
+                        break;
+                    }
+                }
+            }
         }
     }
-    var uniqueItemNames = [...new Set(itemNameList)];
-    console.log(uniqueItemNames)
-        for(var i = 0; i < largestName.length; i++) {
-            var tr = document.createElement("tr");
-            select.appendChild(tr);
-            var opt = largestName[i];
-            var el = document.createElement("td");
-            el.textContent = opt;
-            el.value = opt;
-            select.appendChild(el);
-            opt = largestPrice1[i]
-            addData(el, opt, select)
-            opt = largestPrice10[i]
-            addData(el, opt, select)
-            opt = largestPrice100[i]
-            addData(el, opt, select)
-            opt = largestCostForOne[i]
-            addData(el, opt, select)
-            opt = largestDifference[i]
-            addData(el, opt, select)
-        }
+    for(var i = 0; i < largestName.length; i++) {
+        var tr = document.createElement("tr");
+        select.appendChild(tr);
+        var opt = largestName[i];
+        var el = document.createElement("td");
+        el.textContent = opt;
+        el.value = opt;
+        select.appendChild(el);
+        opt = largestPrice1[i]
+        addData(el, opt, select)
+        opt = largestPrice10[i]
+        addData(el, opt, select)
+        opt = largestPrice100[i]
+        addData(el, opt, select)
+        opt = largestCostForOne[i]
+        addData(el, opt, select)
+        opt = largestDifference[i]
+        addData(el, opt, select)
+    }
+    var tenTable = document.getElementById("tenItemTable");
+    for(var i = 0; i < tenLargestName.length; i++) {
+        var tr = document.createElement("tr");
+        tenTable.appendChild(tr);
+        var opt = tenLargestName[i];
+        var el = document.createElement("td");
+        el.textContent = opt;
+        el.value = opt;
+        tenTable.appendChild(el);
+        opt = tenLargestPrice1[i]
+        addData(el, opt, tenTable)
+        opt = tenLargestPrice10[i]
+        addData(el, opt, tenTable)
+        opt = tenLargestPrice100[i]
+        addData(el, opt, tenTable)
+        opt = tenLargestCostForOne[i]
+        addData(el, opt, tenTable)
+        opt = tenLargestDifference[i]
+        addData(el, opt, tenTable)
+    }
     });
 
 });
 
-function addData(el, data, select) {
+function addData(el, data, selected) {
     el = document.createElement("td");
     el.textContent = data;
     el.value = data;
-    select.appendChild(el);
+    selected.appendChild(el);
 }
